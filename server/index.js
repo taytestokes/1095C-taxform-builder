3255bbb2cf310c140640d2a3a665fb5d2b858c97
Tayte Stokes <taytestokes9328@gmail.com>
2019-11-15 18:06:42 -0700

    setup form upload endpoint

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 8080;
+const IncomingForm = require("formidable").IncomingForm;
 
 // App Initialization
 const app = express();
 
+// Options
+const corsOptions = {
+  origin: "*",
+  optionsSucessStatus: 200
+};
+
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+
+// Endpoints
+app.post("/upload", (req, res) => {
+  const form = new IncomingForm();
+
+  form.on("file", (field, file) => {});
+
+  form.on("end", () => {
+    res.json("Sucess");
+  });
+
+  form.parse(req);
+});
 
 // API Listening
 app.listen(port, () => console.log(`Server running on port: ${port}`));
