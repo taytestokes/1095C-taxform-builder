145f3d586b62e843b62758e44bd57b84519edf6c
Tayte Stokes <35533279+TayteStokes@users.noreply.github.com>
2019-12-11 22:02:15 -0700

    Delete router.js

--- server/routers/router.js
+++
@@ -1,9 +0,0 @@
-// Routers
-const { AuthRouter } = require("./Auth");
-const { UploadRouter } = require("./Documents");
-
-// Add Routes To Server
-exports.addRoutes = app => {
-  app.use("/auth", AuthRouter);
-  app.use("/documents", UploadRouter);
-};
