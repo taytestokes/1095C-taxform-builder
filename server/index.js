3c63e402895a7c111747d455e744d02305ebe59d
Tayte Stokes <taytestokes9328@gmail.com>
2019-12-11 21:27:40 -0700

    ready for deploy

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 const middleware = require("./Middleware/Provider");
 const database = require("../db/utils/connect");
 const router = require("./Routers/Router");
+const path = require("path");
 
 // App Initialization
 const app = express();
@@ -17,6 +18,14 @@ database.connect(app);
 
 // Endpoints
 router.addRoutes(app);
+
+// Public Serve Build Folder For Client
+app.use(express.static(`${__dirname}/../build`));
+
+// Static Route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 // API Listening
 app.listen(port, () => console.log(`Server running on port: ${port}`));
