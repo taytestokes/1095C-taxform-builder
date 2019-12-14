80140427ea939d3a732476a7871b8ab76617b382
Tayte Stokes <taytestokes9328@gmail.com>
2019-12-13 19:43:22 -0700

    setup clustering

--- server/index.js
+++ server/index.js
@@ -1,31 +1,63 @@
+/* OS and Node Cluster Modules */
 require("dotenv").config();
-require("./Auth/Passport");
-const express = require("express");
-const port = process.env.PORT || 8080;
-const middleware = require("./Middleware/provider");
-const database = require("../db/utils/connect");
-const router = require("./Routers/Router");
-const path = require("path");
+const cluster = require("cluster");
+const os = require("os");
+const chalk = require("chalk");
+const environment = process.env.NODE_ENV;
 
-// App Initialization
-const app = express();
+/* Check Master Cluster and Create Child Workers */
+if (cluster.isMaster && environment !== 'development') {
+  // Define number of CPU
+  const numOfCpus = os.cpus().length;
 
-// Middleware
-middleware.provider(app);
+  console.log(chalk.green(`Master cluster setting up ${numOfCpus} workers`));
 
-// Database
-database.connect(app);
+  // Create worker for every CPU
+  for (let i = 0; i < numOfCpus; i++) {
+    cluster.fork();
+  }
 
-// Endpoints
-router.addRoutes(app);
+  // Checks for online status
+  cluster.on("online", worker => {
+    console.log(chalk.yellow(`Worker ${worker.process.pid} is online`));
+  });
 
-// Public Serve Build Folder For Client
-app.use(express.static(`${__dirname}/../build`));
+  // Check for exit status
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(chalk.red(`Worker ${worker.process.pid} died with code ${code}, and signal ${signal}`));
+    console.log(chalk.green("Starting a new worker"));
+    cluster.fork();
+  });
 
-// Static Route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
+} else {
+  const express = require("express");
+  const port = process.env.PORT || 8080;
+  const middleware = require("./Middleware/provider");
+  const database = require("../db/utils/connect");
+  const router = require("./Routers/Router");
+  const path = require("path");
+  require("./Auth/Passport");
 
-// API Listening
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+  // App Initialization
+  const app = express();
+
+  // Middleware
+  middleware.provider(app);
+
+  // Database
+  database.connect(app);
+
+  // Endpoints
+  router.addRoutes(app);
+
+  // Public Serve Build Folder For Client
+  app.use(express.static(`${__dirname}/../build`));
+
+  // Static Route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
+
+  // API Listening
+  app.listen(port, () => console.log(`Server running on port: ${port}`));
+}
